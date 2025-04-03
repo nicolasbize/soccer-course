299691427283142ac8c7c500ad8513f1ca3cd9b3
Nicolas Bize <nicolasbize@gmail.com>
2025-04-02 20:33:09 -0700

    Revert "updated ball to roll with an offset"
    This reverts commit 148803a67db0388d029667f654cc964c218ecf71.

--- scenes/ball/ball_states/ball_state_carried.gd
+++ scenes/ball/ball_states/ball_state_carried.gd
@@ -1,20 +1,8 @@
 class_name BallStateCarried
 extends BallState
 
-const OFFSET_FROM_PLAYER := Vector2(10, 4)
-
 func _enter_tree() -> void:
 	assert(carrier != null)
 
 func _process(_delta: float) -> void:
-	ball.position = carrier.position + Vector2(carrier.heading.x * OFFSET_FROM_PLAYER.x, OFFSET_FROM_PLAYER.y)
-	
-	if carrier.velocity.length_squared() > 0:
-		if carrier.heading.x >= 0:
-			animation_player.play("roll")
-			animation_player.advance(0)
-		elif carrier.heading.x < 0:
-			animation_player.play_backwards("roll")
-			animation_player.advance(0)
-	else:
-		animation_player.play("idle")
+	ball.position = carrier.position
