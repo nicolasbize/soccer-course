79c1af17128e85f64631f4f3736541666c8eabc1
Nicolas Bize <nicolasbize@gmail.com>
2025-04-02 20:32:55 -0700

    Revert "added oscillation to ball movement"
    This reverts commit 6f035477085949e84209791b5b09961e9485a286.

--- scenes/ball/ball_states/ball_state_carried.gd
+++ scenes/ball/ball_states/ball_state_carried.gd
@@ -2,21 +2,14 @@ class_name BallStateCarried
 extends BallState
 
 const OFFSET_FROM_PLAYER := Vector2(10, 4)
-const DRIBBLE_INTENSITY := 3.0
-const DRIBBLE_FREQUENCY := 10.0
-
-var dribble_time := 0.0
 
 func _enter_tree() -> void:
 	assert(carrier != null)
 
-func _process(delta: float) -> void:
-	var vx := 0.0
-	dribble_time += delta
+func _process(_delta: float) -> void:
+	ball.position = carrier.position + Vector2(carrier.heading.x * OFFSET_FROM_PLAYER.x, OFFSET_FROM_PLAYER.y)
 	
 	if carrier.velocity.length_squared() > 0:
-		if abs(carrier.velocity.normalized().y) != 1: # skip move up or down only
-			vx = cos(dribble_time * DRIBBLE_FREQUENCY) * DRIBBLE_INTENSITY
 		if carrier.heading.x >= 0:
 			animation_player.play("roll")
 			animation_player.advance(0)
@@ -25,6 +18,3 @@ func _process(delta: float) -> void:
 			animation_player.advance(0)
 	else:
 		animation_player.play("idle")
-		
-	ball.position = carrier.position + Vector2(vx + carrier.heading.x * OFFSET_FROM_PLAYER.x, OFFSET_FROM_PLAYER.y)
-		
