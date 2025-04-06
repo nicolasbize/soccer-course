0960f26aa2a718f01eb843b6df6bd8031c08f3da
Nicolas Bize <nicolasbize@gmail.com>
2025-04-06 13:20:43 -0700

    added simple camera movement

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,16 +1,16 @@
 class_name Camera
 extends Camera2D
 
-const SMOOTHING_PLAYER := 2
-const SMOOTHING_FREEFORM := 8
+const DISTANCE_TARGET := 100.0
+const SMOOTHING_BALL_CARRIED := 2
+const SMOOTHING_BALL_DEFAULT := 8
 
 @export var ball : Ball
-@export var distance_target : float
 
 func _process(_delta: float) -> void:
 	if ball.carrier != null:
-		position = ball.carrier.position + ball.carrier.heading * distance_target
-		position_smoothing_speed = SMOOTHING_PLAYER
+		position = ball.carrier.position + ball.carrier.heading * DISTANCE_TARGET
+		position_smoothing_speed = SMOOTHING_BALL_CARRIED
 	else:
 		position = ball.position
-		position_smoothing_speed = SMOOTHING_FREEFORM
+		position_smoothing_speed = SMOOTHING_BALL_DEFAULT
