f34d283636248d3dc2147a6ca1b37b8a80832c91
Nicolas Bize <nicolasbize@gmail.com>
2025-05-07 08:55:47 -0700

    added random celebration

--- scenes/characters/character_states/player_state_celebrating.gd
+++ scenes/characters/character_states/player_state_celebrating.gd
@@ -1,15 +1,17 @@
 class_name PlayerStateCelebrating
 extends PlayerState
 
-const AIR_FRICTION := 35.0
+const AIR_FRICTION := 60.0
 const CELEBRATING_HEIGHT := 2.0
 
+var initial_delay := randi_range(200, 500)
+var time_since_celebrating := Time.get_ticks_msec()
+
 func _enter_tree() -> void:
-	celebrate()
 	GameEvents.team_reset.connect(on_team_reset.bind())
 
 func _process(delta: float) -> void:
-	if player.height == 0:
+	if player.height == 0 and Time.get_ticks_msec() - time_since_celebrating > initial_delay:
 		celebrate()
 	player.velocity = player.velocity.move_toward(Vector2.ZERO, delta * AIR_FRICTION)
 
