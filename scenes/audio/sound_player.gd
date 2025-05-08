9ba3f3e04ea3e806e85f7a332e0a3043a2722149
Nicolas Bize <nicolasbize@gmail.com>
2025-05-07 22:16:39 -0700

    added sound effects and fixed issue with scoring area

--- scenes/audio/sound_player.gd
+++ scenes/audio/sound_player.gd
@@ -2,9 +2,8 @@ extends Node
 
 enum Sound {BOUNCE, HURT, PASS, POWERSHOT, SHOT, TACKLING, UI_NAV, UI_SELECT, WHISTLE}
 
-const DELAY_BETWEEN_SIMILAR_SOUNDS := 100
 const NB_CHANNELS := 4
-const SFX_MAP : Dictionary[Sound, AudioStream] = {
+const SFX_MAP: Dictionary[Sound, AudioStream] = {
 	Sound.BOUNCE: preload("res://assets/sfx/bounce.wav"),
 	Sound.HURT: preload("res://assets/sfx/hurt.wav"),
 	Sound.PASS: preload("res://assets/sfx/pass.wav"),
@@ -17,26 +16,19 @@ const SFX_MAP : Dictionary[Sound, AudioStream] = {
 }
 
 var stream_players : Array[AudioStreamPlayer] = []
-var time_since_played : Dictionary[Sound, int] = {}
 
 func _ready() -> void:
-	process_mode = Node.PROCESS_MODE_ALWAYS
 	for i in range(NB_CHANNELS):
 		var stream_player := AudioStreamPlayer.new()
 		stream_players.append(stream_player)
 		add_child(stream_player)
-	for i in range(Sound.size()):
-		time_since_played.set(i as Sound, Time.get_ticks_msec())
 
 func play(sound: Sound) -> void:
-	if Time.get_ticks_msec() - time_since_played.get(sound) < DELAY_BETWEEN_SIMILAR_SOUNDS:
-		return
 	var stream_player := find_first_available_player()
 	if stream_player != null:
-		time_since_played.set(sound, Time.get_ticks_msec())
 		stream_player.stream = SFX_MAP[sound]
 		stream_player.play()
-	
+
 func find_first_available_player() -> AudioStreamPlayer:
 	for stream_player in stream_players:
 		if not stream_player.playing:
--- scenes/game_manager/game_states/game_state_kickoff.gd
+++ scenes/game_manager/game_states/game_state_kickoff.gd
@@ -17,6 +17,6 @@ func _enter_tree() -> void:
 func _process(_delta: float) -> void:
 	for control_scheme : Player.ControlScheme in valid_control_schemes:
 		if KeyUtils.is_action_just_pressed(control_scheme, KeyUtils.Action.PASS):
-			SoundPlayer.play(SoundPlayer.Sound.WHISTLE)
 			GameEvents.kickoff_started.emit()
+			SoundPlayer.play(SoundPlayer.Sound.WHISTLE)
 			transition_state(GameManager.State.IN_PLAY)
