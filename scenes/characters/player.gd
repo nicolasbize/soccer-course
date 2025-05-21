222632ea1527212f2d3de0a11cec90f3d14cf810
Nicolas Bize <nicolasbize@gmail.com>
2025-05-20 21:31:43 -0700

    added match class

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -62,7 +62,7 @@ func _ready() -> void:
 	spawn_position = position
 	GameEvents.team_scored.connect(on_team_scored.bind())
 	GameEvents.game_over.connect(on_game_over.bind())
-	var initial_position := kickoff_position if country == GameManager.countries[0] else spawn_position
+	var initial_position := kickoff_position if country == GameManager.current_match.country_home else spawn_position
 	switch_state(State.RESETING, PlayerStateData.build().set_reset_position(initial_position))
 
 func _process(delta: float) -> void:
--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -26,11 +26,12 @@ func _process(_delta: float) -> void:
 	update_clock()
 
 func update_score() -> void:
-	score_label.text = ScoreHelper.get_score_text(GameManager.score)
+	score_label.text = ScoreHelper.get_score_text(GameManager.current_match)
 
 func update_flags() -> void:
 	for i in flag_textures.size():
-		flag_textures[i].texture = FlagHelper.get_texture(GameManager.countries[i])
+		var countries := [GameManager.current_match.country_home, GameManager.current_match.country_away]
+		flag_textures[i].texture = FlagHelper.get_texture(countries[i])
 
 func update_clock() -> void:
 	if GameManager.time_left < 0:
@@ -47,14 +48,14 @@ func on_ball_released() -> void:
 func on_score_changed() -> void:
 	if not GameManager.is_time_up():
 		goal_scorer_label.text = "%s SCORED!" % [last_ball_carrier]
-		score_info_label.text = ScoreHelper.get_current_score_info(GameManager.countries, GameManager.score)
+		score_info_label.text = ScoreHelper.get_current_score_info(GameManager.current_match)
 		animation_player.play("goal_appear")
 	update_score()
 
 func on_team_reset() -> void:
-	if GameManager.has_someone_scored():
+	if GameManager.current_match.has_someone_scored():
 		animation_player.play("goal_hide")
 
 func on_game_over(_country_winner: String) -> void:
-	score_info_label.text = ScoreHelper.get_final_score_info(GameManager.countries, GameManager.score)
+	score_info_label.text = ScoreHelper.get_final_score_info(GameManager.current_match)
 	animation_player.play("game_over")
